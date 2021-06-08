e725066f368c09e37af095e1059dd1a97a6c2dc9
Andrii Velychko <velychko.andrii@gmail.com>
2021-06-08 14:17:53 +0200

    Provide three different ways to complete the task

--- src/main/java/ua/andvel/preacademystudent/Main.java
+++ src/main/java/ua/andvel/preacademystudent/Main.java
@@ -1,42 +1,116 @@
 package ua.andvel.preacademystudent;
 
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 /**
  * @author Andrii Velychko
  */
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         StudentApp app = new StudentApp();
-//        PreAcademyStudent[] preAcademyStudents = app.createStudents(sc, 4);
-        PreAcademyStudent[] preAcademyStudents = {
-                new PreAcademyStudent("A", 2, 3, 4),
-                new PreAcademyStudent("B", 2, 1, 4),
-                new PreAcademyStudent("C", 5, 3, 2),
-                new PreAcademyStudent("D", 4, 2, 3),
+
+//        PreAcademyStudent[] preAcademyStudents = app.getStudentsFromConsole(4);
+        PreAcademyStudent[] preAcademyStudents = createSevenPreAcademyStudents();
+
+        printOrderedStudentsUsingFourOrderMethods(app, preAcademyStudents);
+
+        printOrderedStudentsUsingOneOrderMethod(app, preAcademyStudents);
+
+        printOrderedStudentsUsingMethodsChaining(app, preAcademyStudents);
+    }
+
+    private static PreAcademyStudent[] createSevenPreAcademyStudents() {
+        return new PreAcademyStudent[]{
+                new PreAcademyStudent("A", "Z", 2, 3, 4),
+                new PreAcademyStudent("B", "Y", 2, 1, 4),
+                new PreAcademyStudent("C", "X", 5, 3, 2),
+                new PreAcademyStudent("D", "W", 4, 2, 3),
+                new PreAcademyStudent("E", "V", 3, 3, 3),
+                new PreAcademyStudent("F", "U", 2, 4, 5),
+                new PreAcademyStudent("G", "T", 3, 2, 4),
         };
+    }
 
-        System.out.println("Grading per all points: ");
-        PreAcademyStudent[] gradedStudentsPerAllPoints = app.gradeStudentsPerAllPoints(preAcademyStudents);
-        Arrays.stream(gradedStudentsPerAllPoints).forEach(x -> System.out.println(x));
+    private static void printOrderedStudentsUsingFourOrderMethods(StudentApp app,
+                                                                  PreAcademyStudent[] preAcademyStudents) {
+        System.out.println("\nRunning printOrderedStudentsUsingFourOrderMethods:");
 
-        System.out.println("Grading per lectures points: ");
-        PreAcademyStudent[] gradedStudentsPerLecturesPoints = app.gradeStudentsPerLecturesPoints(preAcademyStudents);
-        Arrays.stream(gradedStudentsPerLecturesPoints).forEach(x -> System.out.println(x));
+        System.out.println("\nGrading per all points: ");
+        PreAcademyStudent[] orderedStudentsPerAllPoints = app.orderStudentsPerAllPoints(preAcademyStudents);
+        Arrays.stream(orderedStudentsPerAllPoints)
+                .forEach(System.out::println);
 
-        System.out.println("Grading per tasks points: ");
-        PreAcademyStudent[] gradedStudentsPerTasksPoints = app.gradeStudentsPerTasksPoints(preAcademyStudents);
-        Arrays.stream(gradedStudentsPerTasksPoints).forEach(x -> System.out.println(x));
+        System.out.println("\nGrading per lectures points: ");
+        PreAcademyStudent[] orderedStudentsPerLecturesPoints = app.orderStudentsPerLecturesPoints(preAcademyStudents);
+        Arrays.stream(orderedStudentsPerLecturesPoints)
+                .forEach(System.out::println);
 
-        System.out.println("Grading per activity points: ");
-        PreAcademyStudent[] gradedStudentsPerActivityPoints = app.gradeStudentsPerActivityPoints(preAcademyStudents);
-        Arrays.stream(gradedStudentsPerActivityPoints).forEach(x -> System.out.println(x));
+        System.out.println("\nGrading per tasks points: ");
+        PreAcademyStudent[] orderedStudentsPerTasksPoints = app.orderStudentsPerTasksPoints(preAcademyStudents);
+        Arrays.stream(orderedStudentsPerTasksPoints)
+                .forEach(System.out::println);
 
+        System.out.println("\nGrading per activity points: ");
+        PreAcademyStudent[] orderedStudentsPerActivityPoints = app.orderStudentsPerActivityPoints(preAcademyStudents);
+        Arrays.stream(orderedStudentsPerActivityPoints)
+                .forEach(System.out::println);
+    }
 
+    private static void printOrderedStudentsUsingOneOrderMethod(StudentApp app,
+                                                                PreAcademyStudent[] preAcademyStudents) {
+        System.out.println("\nRunning printOrderedStudentsUsingOneOrderMethod:");
 
+        System.out.println("\nGrading per all points: ");
+        PreAcademyStudent[] orderedStudentsPerAllPoints = app.orderStudentsPer(PreAcademyStudent::totalPoints,
+                preAcademyStudents);
+        Arrays.stream(orderedStudentsPerAllPoints)
+                .forEach(System.out::println);
 
+        System.out.println("\nGrading per lectures points: ");
+        PreAcademyStudent[] orderedStudentsPerLecturesPoints = app.orderStudentsPer(PreAcademyStudent::lecturesPoints,
+                preAcademyStudents);
+        Arrays.stream(orderedStudentsPerLecturesPoints)
+                .forEach(System.out::println);
+
+        System.out.println("\nGrading per tasks points: ");
+        PreAcademyStudent[] orderedStudentsPerTasksPoints = app.orderStudentsPer(PreAcademyStudent::tasksPoints,
+                preAcademyStudents);
+        Arrays.stream(orderedStudentsPerTasksPoints)
+                .forEach(System.out::println);
+
+        System.out.println("\nGrading per activity points: ");
+        PreAcademyStudent[] orderedStudentsPerActivityPoints = app.orderStudentsPer(PreAcademyStudent::activityPoints,
+                preAcademyStudents);
+        Arrays.stream(orderedStudentsPerActivityPoints)
+                .forEach(System.out::println);
+    }
+
+    private static void printOrderedStudentsUsingMethodsChaining(StudentApp app,
+                                                                 PreAcademyStudent[] preAcademyStudents) {
+        System.out.println("\nRunning printOrderedStudentsUsingMethodsChaining:");
+
+        System.out.println("\nGrading per all points: ");
+        PreAcademyStudent[] orderedStudentsPerAllPoints = app.order(preAcademyStudents)
+                .per(PreAcademyStudent::totalPoints);
+        Arrays.stream(orderedStudentsPerAllPoints)
+                .forEach(System.out::println);
+
+        System.out.println("\nGrading per lectures points: ");
+        PreAcademyStudent[] orderedStudentsPerLecturesPoints = app.order(preAcademyStudents)
+                .per(PreAcademyStudent::lecturesPoints);
+        Arrays.stream(orderedStudentsPerLecturesPoints)
+                .forEach(System.out::println);
+
+        System.out.println("\nGrading per tasks points: ");
+        PreAcademyStudent[] orderedStudentsPerTasksPoints = app.order(preAcademyStudents)
+                .per(PreAcademyStudent::tasksPoints);
+        Arrays.stream(orderedStudentsPerTasksPoints)
+                .forEach(System.out::println);
+
+        System.out.println("\nGrading per activity points: ");
+        PreAcademyStudent[] orderedStudentsPerActivityPoints = app.order(preAcademyStudents)
+                .per(PreAcademyStudent::activityPoints);
+        Arrays.stream(orderedStudentsPerActivityPoints)
+                .forEach(System.out::println);
     }
 }
--- src/main/java/ua/andvel/preacademystudent/StudentApp.java
+++ src/main/java/ua/andvel/preacademystudent/StudentApp.java
@@ -1,36 +1,44 @@
 package ua.andvel.preacademystudent;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.function.ToIntFunction;
 
 /**
  * @author Andrii Velychko
  */
 class StudentApp {
 
-    PreAcademyStudent[] createStudents(Scanner sc, int times) {
+    private PreAcademyStudent[] studentsToSort;
+
+    /**
+     * Get data needed for {@link PreAcademyStudent} creation.
+     *
+     * @param times number of students to ask data for
+     * @return      array of students
+     */
+    PreAcademyStudent[] getStudentsFromConsole(int times) {
+        Scanner sc = new Scanner(System.in);
         PreAcademyStudent[] preAcademyStudents = new PreAcademyStudent[times];
         for (int i = 0; i < times; i++) {
-            String name = getFromConsoleStudentName(sc, i + 1);
-            double lecturePoints = getFromConsolePointsFor("lectures", sc, i + 1);
-            double tasksPoints = getFromConsolePointsFor("tasks", sc, i + 1);
-            double activityPoints = getFromConsolePointsFor("activity", sc, i + 1);
-            preAcademyStudents[i] = new PreAcademyStudent(name, lecturePoints, tasksPoints, activityPoints);
+            String firstName = getFromConsoleStudentFirstName(sc, i + 1);
+            String lastName = getFromConsoleStudentLastName(sc, i + 1);
+            int lecturePoints = getFromConsolePointsFor("lectures", sc, i + 1);
+            int tasksPoints = getFromConsolePointsFor("tasks", sc, i + 1);
+            int activityPoints = getFromConsolePointsFor("activity", sc, i + 1);
+            preAcademyStudents[i] = new PreAcademyStudent(firstName, lastName, lecturePoints, tasksPoints,
+                    activityPoints);
         }
         return preAcademyStudents;
     }
 
-    private double getFromConsolePointsFor(String activityName, Scanner sc, int studentNumber) {
+    private int getFromConsolePointsFor(String activityName, Scanner sc, int studentNumber) {
         boolean validNumber = false;
-        double points = 0;
+        int points = 0;
 
         while (!validNumber) {
             System.out.printf("Enter student %d points for %s:%n", studentNumber, activityName);
             try {
-                points = sc.nextDouble();
+                points = sc.nextInt();
                 validNumber = true;
             } catch (InputMismatchException ime) {
                 System.err.println("Entered value is not a valid number!");
@@ -41,40 +49,106 @@ class StudentApp {
         return points;
     }
 
-    private String getFromConsoleStudentName(Scanner sc, int id) {
-        System.out.printf("Enter student %d name:%n", id);
+    private String getFromConsoleStudentFirstName(Scanner sc, int id) {
+        System.out.printf("Enter student %d first name:%n", id);
         return sc.nextLine();
     }
 
-    // If students.length is 10 then best grade is 10
-    private void gradeStudentsBySorting(PreAcademyStudent[] students) {
-        IntStream.rangeClosed(0, students.length - 1)
-                .forEach(num -> students[num].setFinalGrade(num + 1));
+    private String getFromConsoleStudentLastName(Scanner sc, int id) {
+        System.out.printf("Enter student %d last name:%n", id);
+        return sc.nextLine();
     }
 
-    private PreAcademyStudent[] gradeStudentsWithComparator(PreAcademyStudent[] students,
-                                                            Comparator<PreAcademyStudent> comp) {
-        Arrays.sort(students, comp);
-        gradeStudentsBySorting(students);
-        return students;
+    /**
+     * Sorts an array of {@link PreAcademyStudent} according to provided function. Sorting is done in reversed
+     * order, where students with higher score appear at the beginning of the array.
+     *
+     * @param function a function that retrieves type of points based on which the sorting should be done
+     * @return         sorted array
+     */
+    PreAcademyStudent[] orderStudentsPer(ToIntFunction<? super PreAcademyStudent> function, PreAcademyStudent[] preAcademyStudents) {
+        Comparator<PreAcademyStudent> comparator = PreAcademyStudent.getComparatorFor(function);
+        Arrays.sort(preAcademyStudents, Collections.reverseOrder(comparator));
+        return preAcademyStudents;
     }
 
-    PreAcademyStudent[] gradeStudentsPerAllPoints(PreAcademyStudent[] students) {
-        Arrays.sort(students);
-        gradeStudentsBySorting(students);
-        return students;
+    /**
+     * Sorts students per their total points. Sorting is done in reversed order, where students with higher score
+     * appear at the beginning of the array.
+     *
+     * @param preAcademyStudents array to be sorted
+     * @return                   sorted array
+     */
+    PreAcademyStudent[] orderStudentsPerAllPoints(PreAcademyStudent[] preAcademyStudents) {
+        Comparator<PreAcademyStudent> comparator = PreAcademyStudent.getComparatorFor(PreAcademyStudent::totalPoints);
+        Arrays.sort(preAcademyStudents, Collections.reverseOrder(comparator));
+        return preAcademyStudents;
     }
 
-    PreAcademyStudent[] gradeStudentsPerLecturesPoints(PreAcademyStudent[] students) {
-        return gradeStudentsWithComparator(students, PreAcademyStudent.getComparatorPerLecturePoints());
+    /**
+     * Sorts students per their lectures points. Sorting is done in reversed order, where students with higher score
+     * appear at the beginning of the array.
+     *
+     * @param preAcademyStudents array to be sorted
+     * @return                   sorted array
+     */
+    PreAcademyStudent[] orderStudentsPerLecturesPoints(PreAcademyStudent[] preAcademyStudents) {
+        Comparator<PreAcademyStudent> comparator = PreAcademyStudent.getComparatorFor(PreAcademyStudent::lecturesPoints);
+        Arrays.sort(preAcademyStudents, Collections.reverseOrder(comparator));
+        return preAcademyStudents;
     }
 
-
-    PreAcademyStudent[] gradeStudentsPerTasksPoints(PreAcademyStudent[] students) {
-        return gradeStudentsWithComparator(students, PreAcademyStudent.getComparatorPerTasksPoints());
+    /**
+     * Sorts students per their tasks points. Sorting is done in reversed order, where students with higher score
+     * appear at the beginning of the array.
+     *
+     * @param preAcademyStudents array to be sorted
+     * @return                   sorted array
+     */
+    PreAcademyStudent[] orderStudentsPerTasksPoints(PreAcademyStudent[] preAcademyStudents) {
+        Comparator<PreAcademyStudent> comparator = PreAcademyStudent.getComparatorFor(PreAcademyStudent::tasksPoints);
+        Arrays.sort(preAcademyStudents, Collections.reverseOrder(comparator));
+        return preAcademyStudents;
     }
 
-    PreAcademyStudent[] gradeStudentsPerActivityPoints(PreAcademyStudent[] students) {
-        return gradeStudentsWithComparator(students, PreAcademyStudent.getComparatorPerActivityPoints());
+    /**
+     * Sorts students per their activity points. Sorting is done in reversed order, where students with higher score
+     * appear at the beginning of the array.
+     *
+     * @param preAcademyStudents array to be sorted
+     * @return                   sorted array
+     */
+    PreAcademyStudent[] orderStudentsPerActivityPoints(PreAcademyStudent[] preAcademyStudents) {
+        Comparator<PreAcademyStudent> comparator = PreAcademyStudent.getComparatorFor(PreAcademyStudent::activityPoints);
+        Arrays.sort(preAcademyStudents, Collections.reverseOrder(comparator));
+        return preAcademyStudents;
+    }
+
+    /**
+     * Sets the the array to sort. This method should be used in conjunction with
+     * {@link StudentApp#per(ToIntFunction)}
+     *
+     * @param  preAcademyStudents array that should be later sorted
+     * @return                    same object on which the method was called
+     */
+    StudentApp order(PreAcademyStudent[] preAcademyStudents) {
+        studentsToSort = preAcademyStudents;
+        return this;
+    }
+
+    /**
+     * Sorts students according to provided function. Sorting is done in reversed order, where students with higher
+     * score appear at the beginning of the array.
+     * This method is similar to {@link StudentApp#orderStudentsPer(ToIntFunction, PreAcademyStudent[])}, however the
+     * {@link StudentApp#order(PreAcademyStudent[])} method should be called first to specify the array of
+     * {@link PreAcademyStudent} to sort.
+     *
+     * @param function a function that retrieves type of points based on which the sorting should be done
+     * @return         sorted array
+     */
+    PreAcademyStudent[] per(ToIntFunction<? super PreAcademyStudent> function) {
+        Comparator<PreAcademyStudent> comparator = PreAcademyStudent.getComparatorFor(function);
+        Arrays.sort(studentsToSort, Collections.reverseOrder(comparator));
+        return studentsToSort;
     }
 }
